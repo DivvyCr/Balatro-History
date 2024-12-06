7bb6ca5f70f3fdade7441ce49112afb83a925c55
Divvy C. <54956345+DivvyCr@users.noreply.github.com>
2024-12-06 15:49:54 +0000

    Add run summary when hovering over stored run button; restructure.

--- src/Init.lua
+++ src/Init.lua
@@ -5,6 +5,7 @@
 if not DV then DV = {} end
 
 DV.HIST = {
+   -- TODO: Move key data into `G.GAME.DV`?
    history = {},
    view = {
       abs_round = 1,
@@ -22,3 +23,32 @@ DV.HIST = {
       SHOP = 3
    }
 }
+
+DV.HIST._start_run = Game.start_run
+function Game:start_run(args)
+   DV.HIST._start_run(self, args)
+   if not args.savetext then
+      -- New run, so modify `GAME` table with custom storage:
+      if not G.GAME.DV then G.GAME.DV = {} end
+      G.GAME.DV.run_id = DV.HIST.simple_uuid()
+
+      -- ...and reset mod data:
+      DV.HIST.history = {}
+      DV.HIST.latest = {
+         rel_round = 0,
+         abs_round = 0,
+         ante = 0,
+      }
+   else
+      -- Loaded run, so extract mod data from loaded data:
+      DV.HIST.history = G.GAME.DV.history
+      DV.HIST.latest = G.GAME.DV.latest
+   end
+end
+
+DV.HIST._save_run = save_run
+function save_run()
+   G.GAME.DV.history = DV.HIST.history
+   G.GAME.DV.latest = DV.HIST.latest
+   DV.HIST._save_run()
+end
---
+++ src/UI/ButtonCallbacks.lua
@@ -0,0 +1,121 @@
+--- Divvy's History for Balatro - UI/ButtonCallbacks.lua
+--
+-- Functions responsible for processing user's actions.
+
+--
+-- RUN HISTORY:
+--
+
+function G.FUNCS.dv_hist_change_view(e)
+   if not e then return end
+
+   if e.config.dir == "fwd" then
+      if DV.HIST.view.abs_round + 1 > DV.HIST.latest.abs_round then return end
+      DV.HIST.view.abs_round = DV.HIST.view.abs_round + 1
+   elseif e.config.dir == "bwd" then
+      if DV.HIST.view.abs_round - 1 < 1 then return end
+      DV.HIST.view.abs_round = DV.HIST.view.abs_round - 1
+   else
+      return
+   end
+
+   local new_ante = math.ceil(DV.HIST.view.abs_round / 3)
+   local new_round = DV.HIST.view.abs_round - (new_ante-1)*3
+
+   DV.HIST.view.text[4] = "Ante " .. new_ante
+
+   -- e.UIBox is the button's UIBox; its parent UIBox is the whole history view.
+   -- The button UIBox is needed to be able to show/hide the buttons at the edge rounds.
+   local outer_uibox = e.UIBox.parent.UIBox
+   local hands_view = outer_uibox:get_UIE_by_ID("dv_hist")
+
+   hands_view.config.object:remove()
+   hands_view.config.object = UIBox({
+      definition = DV.HIST.get_content({ante_num = new_ante, rel_round_num = new_round}),
+      config = {parent = hands_view, type = "cm"}
+   })
+
+   local hands_view_wrap = outer_uibox:get_UIE_by_ID("dv_hist_align")
+   hands_view_wrap.config.align = DV.HIST.get_content_alignment(new_ante, new_round)
+
+   DV.HIST.update_pips(outer_uibox, new_ante)
+   DV.HIST.update_buttons(outer_uibox)
+
+   outer_uibox:recalculate()
+end
+
+function DV.HIST.update_pips(hist_uibox, ante)
+   if not hist_uibox then return end
+
+   for i = 1, 3 do
+      local abs_round_num = (ante-1)*3 + i
+      if abs_round_num > DV.HIST.latest.abs_round then
+         DV.HIST.view.text[i] = " "
+      else
+         DV.HIST.view.text[i] = "Round " .. abs_round_num
+      end
+
+      local pip = hist_uibox:get_UIE_by_ID("dv_hist_round_" .. i)
+      if abs_round_num > DV.HIST.latest.abs_round then
+         pip.parent.config.colour = G.C.GREY
+      elseif i == (DV.HIST.view.abs_round - (ante-1)*3) then
+         pip.parent.config.colour = G.C.RED
+      else
+         pip.parent.config.colour = G.C.BLACK
+      end
+   end
+end
+
+function DV.HIST.update_buttons(hist_uibox)
+   local fwd_button = hist_uibox:get_UIE_by_ID("dv_hist_fwd_but")
+   if DV.HIST.view.abs_round >= DV.HIST.latest.abs_round then
+     fwd_button.config.object:remove()
+   end
+   fwd_button.config.object = UIBox({
+     definition = DV.HIST.create_button("fwd"),
+     config = {parent = fwd_button, type = "cm"}
+   })
+
+   local bwd_button = hist_uibox:get_UIE_by_ID("dv_hist_bwd_but")
+   if DV.HIST.view.abs_round <= 1 then
+      bwd_button.config.object:remove()
+   end
+   bwd_button.config.object = UIBox({
+     definition = DV.HIST.create_button("bwd"),
+     config = {parent = bwd_button, type = "cm"}
+   })
+end
+
+--
+-- RUN STORAGE:
+--
+
+function G.FUNCS.dv_hist_select_run(e)
+   if not e then return end
+   G.FUNCS.overlay_menu({
+         definition = G.UIDEF.stored_runs()
+   })
+end
+
+function G.FUNCS.dv_hist_update_runs_page(args)
+   if not args or not args.cycle_config then return end
+   local callback_args = args.cycle_config.dv
+
+   local runs_object = callback_args.ui
+   local runs_wrap = runs_object.parent
+
+   runs_wrap.config.object:remove()
+   runs_wrap.config.object = UIBox({
+         definition = DV.HIST.get_stored_runs_page({run_paths = callback_args.rds, runs_per_page = callback_args.rpp, page_num = args.to_key}),
+         config = {parent = runs_wrap, type = "cm"}
+   })
+   runs_wrap.UIBox:recalculate()
+end
+
+function G.FUNCS.dv_hist_load_run(e)
+   if not e then return end
+
+   G.SAVED_GAME = e.config.ref_table.run_data
+
+   G.FUNCS.setup_run(e)
+end
